cc596a0d46b1a62441908e0e666a4d749b4f2e03
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-08 15:12:26 +0000

    automatically backed up by learn

---
+++ shake_it_off.rb
@@ -0,0 +1,25 @@
+# puts "cause the players gonna
+# puts "play" 5x
+# puts "and the hater's gonna"
+# puts "hate" 5x
+# puts "Baby I'm just gonna..."
+
+def shake_it_off
+  puts "cause the player's gonna"
+  play_count = 0 
+  while play_count < 6 
+  puts "play"
+  play_count += 1 
+end 
+
+  puts "and the hater's gonna"
+  hate_count = 0 
+  while hate_count < 6
+  puts "hate"
+  hate_count += 1 
+end 
+
+  puts "Baby I'm just gonna..."
+end 
+
+shake_it_off 
